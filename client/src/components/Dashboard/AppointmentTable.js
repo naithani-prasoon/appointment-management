084c9b1014b526fb4842ccb2e3a6b7a197a4b86a
Prasoon Naithani <62165346+naithani-prasoon@users.noreply.github.com>
2022-07-14 16:30:26 -0500

    Dashboard almost completed (closing popup left)

--- client/src/components/Dashboard/AppointmentTable.js
+++ client/src/components/Dashboard/AppointmentTable.js
@@ -2,13 +2,16 @@ import React from "react";
 import axios from "axios";
 import './dashboard.css';
 import AppointmentForm from "./AppointmentForm";
+import EditIcon from '../Assets/edit.svg'
+import DeleteIcon from '../Assets/delete.svg'
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+function createData(name, type, description, start, end) {
+    return { name, type, description, start, end };
 }
 
+//trial data
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Frozen yoghurt', "159", 0, 24, 4.0),
     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
     createData('Eclair', 262, 16.0, 24, 6.0),
     createData('Cupcake', 305, 3.7, 67, 4.3),
@@ -16,12 +19,15 @@ const rows = [
 ];
 
 export default function AppointmentTable() {
+
+    //appointment data
     const baseUrl = 'http://localhost:8080/api/v1/appointment/'
     const [appointmentList, setAppointmentList] = React.useState([]);
-    const [popup, setPopup] = React.useState(false);
-    const [editAppoitment, setEditAppointment] = React.useState({});
 
-    const[create, setCreate] = React.useState(false)
+    //pop up states
+    const [popup, setPopup] = React.useState(false);
+    const [createAppointment, setCreateAppointment] = React.useState(false);
+    const [editAppoitment, setEditAppointment] = React.useState({name:'', type:'', description:'', start:'', end:''});
 
     function handleEditClick(appointment){
         setEditAppointment(appointment);
@@ -29,7 +35,8 @@ export default function AppointmentTable() {
     }
 
     function handleCreateAppointment(){
-        setCreate(true);
+        setPopup(true);
+        setCreateAppointment(true);
     }
 
     React.useEffect(() => {
@@ -44,54 +51,38 @@ export default function AppointmentTable() {
                     <button onClick={() => handleCreateAppointment()}> Create Appointment </button>
                 </div>
                 <div className="table-header">
-                    <h2 style={{textAlign:"left"}}> Name </h2>
+
+                    <h2> Name </h2>
                     <h2> Type </h2>
                     <h2> Description </h2>
                     <h2> Start Time </h2>
                     <h2> End Time </h2>
+                    <h2></h2>
                     <h2></h2>
                 </div>
 
                 {rows.map((row) => (
                     <div className="table-appointments">
                         <h2 style={{textAlign:"left"}}>{row.name}</h2>
-                        <h2>{row.calories}</h2>
-                        <h2>{row.fat}</h2>
-                        <h2>{row.carbs}</h2>
-                        <h2>{row.protein}</h2>
-                        <h2 onClick={() => handleEditClick(row)}> EDIT </h2>
+                        <h2>{row.type}</h2>
+                        <h2 style={{textAlign:"left"}}>{row.description}</h2>
+                        <h2>{row.start}</h2>
+                        <h2>{row.end}</h2>
+                        <img src={EditIcon} onClick={() => handleEditClick(row)}/>
+                        <img src={DeleteIcon}/>
                     </div>
                 ))}
                 {
                     popup ?
                         <div className="edit-popup">
                             <div className="popup">
-                                <AppointmentForm/>
+                                {createAppointment ? <AppointmentForm edit={false} data={editAppoitment}/> :
+                                    <AppointmentForm edit={true} data ={editAppoitment}/>}
                             </div>
                         </div>
                         :
                         <></>
                 }
-
-                {
-                    create ?
-                        <div className="edit-popup">
-                            <div className="popup" >
-                                <form>
-                                    <input type={"text"}/>
-                                    <input type={"text"}/>
-                                    <textarea type={"text"}/>
-                                    <input type={"time"}/>
-                                    <input type={"time"}/>
-                                    <button type={"submit"}>Create</button>
-                                    <button onClick={() => setCreate(false)} > Close </button>
-                                </form>
-                            </div>
-                        </div>
-                        :
-                        <></>
-                }
-
             </div>
     )
 }
--- client/src/routes/dashboard.js
+++ client/src/routes/dashboard.js
@@ -1,11 +1,12 @@
 import Navigation from "../components/Navigation"
 import AppointmentTable from "../components/Dashboard/AppointmentTable"
+import '../components/Dashboard/dashboard.css'
 
 export default function Dashboard() {
     return(
         <>
             <Navigation />
-            <h1>Dashboard</h1>
+            <h1> User's Dashboard</h1>
             <div>
                 <AppointmentTable/>
             </div>
